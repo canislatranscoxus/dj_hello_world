09abfc369717d0026a52b089d913be865ebd3784
canislatranscoxus <canislatranscoxus@gmail.com>
2020-12-19 19:38:41 -0600

    my_app/urls.py

--- my_proj/my_app/urls.py
+++ my_proj/my_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls        import path
-from my_app     import views
+from django.urls    import path
+from my_app         import views
 
-
+app_name = 'my_app'
 urlpatterns = [
-    path( ''                , views.hello         ),
-    path( 'hello/'          , views.hello         ),
-    path( 'get_date_time/'  , views.get_date_time )
+    path( ''                , views.hello        , name= 'index' ),
+    path( 'hello/'          , views.hello        , name= 'hello' ),
+    path( 'get_date_time/'  , views.get_date_time, name= 'get_date_time' )
 
 ]
